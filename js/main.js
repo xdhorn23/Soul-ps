0d4156a79e6a1a429bd0f3c11f011001ff9ee452
xdhorn23 <xdhorn123@gmail.com>
2026-07-01 19:51:21 +0700

    Update URLs to point to .txt files

--- js/main.js
+++ js/main.js
@@ -96,14 +96,14 @@ function downloadHost() {
 }
 
 function copyPowerTunnelURL() {
-  const url = "https://soulps.vercel.app/ptunnel";
+  const url = "https://soulps.vercel.app/ptunnel.txt";
   navigator.clipboard.writeText(url)
     .then(() => showNotification('🔗 PowerTunnel URL disalin!'))
     .catch(() => showNotification('Gagal', 'error'));
 }
 
 function copyIOS() {
-  const url = "https://soulps.vercel.app/iosconf";
+  const url = "https://soulps.vercel.app/iosconf.txt";
   navigator.clipboard.writeText(url)
     .then(() => showNotification('🍎 iOS URL disalin!'))
     .catch(() => showNotification('Gagal', 'error'));
